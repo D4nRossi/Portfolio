cb9afbf5d79543f186420058499d749911ac0fe1
Daniel Rossi <danielrossi5678@gmail.com>
2022-05-05 17:46:08 -0300

    Códigos finais, tudo feito, erros corrigidos e implementações implementadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,16 @@ const images = document.querySelectorAll(".images img");
 const next_btn = document.querySelector(".next-btn");
 const prev_btn = document.querySelector(".prev-btn");
 
+const links = document.querySelectorAll(".nav-link");
+
+const toggle_btn = document.querySelector(".toggle-btn");
+
+const hamburger = document.querySelector(".hamburger");
+
 
 
 function stickyNavbar(){
+    activeLink();
    header.classList.toggle('scrolled', window.pageYOffset > 0);
 }
 window.addEventListener("scroll",stickyNavbar);
@@ -144,9 +151,58 @@ function changeImage(index){
     images[index].classList.add("showImage");
 }
 
-/*document.getElementById('mail').addEventListener('submit', function () {
-    var nome = this.querySelector('input[name=nome]'), nome = nome.value;
-    var email = this.querySelector('input[name=email]'), email = email.value;
-    var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
-    this.querySelector('textarea[name=Body]').setAttribute('value', texto);
-});*/
+
+
+function activeLink(){
+    let sections = document.querySelectorAll("section[id]");
+    let passedSections = Array.from(sections).map((sct, i) => {
+        return{
+            y: sct.getBoundingClientRect().top - header.offsetHeight,
+            id: i,
+        };
+    }).filter((sct) => sct.y <= 0);
+    let currSectionID = passedSections.at(-1).id;
+    
+    links.forEach((l) => l.classList.remove("active"));
+    links[currSectionID].classList.add("active");
+}
+
+activeLink();
+
+
+
+let firstTheme = localStorage.getItem("dark");
+
+changeTheme(+firstTheme);
+
+
+function changeTheme(isDark){
+    if(isDark){
+        document.body.classList.add("dark");
+        toggle_btn.classList.replace("uil-moon", "uil-sun");
+        localStorage.setItem("dark", 1);
+    }else{
+        document.body.classList.remove("dark");
+        toggle_btn.classList.replace("uil-sun", "uil-moon");
+        localStorage.setItem("dark", 0);
+    }
+}
+
+toggle_btn.addEventListener("click", () => {
+    changeTheme(!document.body.classList.contains("dark"));
+});
+
+
+
+hamburger.addEventListener("click",()=>{
+    document.body.classList.toggle("open");
+    document.body.classList.toggle("stopScrolling");
+});
+
+links.forEach(link => link.addEventListener("click", () => {
+    document.body.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+}));
+
+
+
