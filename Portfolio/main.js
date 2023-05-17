63f5338ffbd53b2f584358f96fdc99dd5a72a1c5
Daniel Rossi de Amorim <danielrossi5678@gmail.com>
2023-05-17 16:51:25 -0300

    main.js copiado do curso, comentarios na versão anterior

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -31,115 +31,190 @@ import * as dat from 'dat.gui'
 
 const gui = new dat.GUI()
 const world = {
-  plane:{
-    width: 10,
-    height: 10,
-    widthSegments:10,
-    heightSegments:10
-  }
-} 
-
-//Alterando a largura
-gui.add(world.plane, 'width', 1, 20).onChange(generatePlane)
-gui.add(world.plane, 'widthSegments', 1, 20).onChange(generatePlane)
-//Alterando a altura
-gui.add(world.plane, 'height', 1, 20).onChange(generatePlane)
-gui.add(world.plane, 'heightSegments', 1, 20).onChange(generatePlane)
-
-function generatePlane(){
-  planeMesh.geometry.dispose();
-  planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, world.plane.widthSegments, world.plane.heightSegments)
-
-  const {array} = planeMesh.geometry.attributes.position;
-  for(let i = 0; i < array.length; i += 3){
-    //Posições individuas de cada vertice
-    const x = array[i];
-    const y = array[i + 1]; 
-    const z = array[i + 2]; 
-    
-    //Movimentando de forma randomica
-    array[i + 2] = z + Math.random();
+  plane: {
+    width: 400,
+    height: 400,
+    widthSegments: 50,
+    heightSegments: 50
   }
 }
-//Raycaster funciona como uma verificação se o mouse esta em cima do objeto
-const raycaster = new THREE.Raycaster();
-const scene = new THREE.Scene();
-//Camera arguments - field of view, scene aspect ratio, clipping plan
-const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
-//Renderer
-const renderer = new THREE.WebGLRenderer();
+gui.add(world.plane, 'width', 1, 500).onChange(generatePlane)
 
-//Arguments - width, height
-renderer.setSize(innerWidth, innerHeight);
-//Definindo o tamnho
-renderer.setPixelRatio(devicePixelRatio);
+gui.add(world.plane, 'height', 1, 500).onChange(generatePlane)
+gui.add(world.plane, 'widthSegments', 1, 100).onChange(generatePlane)
+gui.add(world.plane, 'heightSegments', 1, 100).onChange(generatePlane)
 
-document.body.appendChild(renderer.domElement);
+function generatePlane() {
+  planeMesh.geometry.dispose()
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  )
 
-//Controle de orbita
+  // vertice position randomization
+  const { array } = planeMesh.geometry.attributes.position
+  const randomValues = []
+  for (let i = 0; i < array.length; i++) {
+    if (i % 3 === 0) {
+      const x = array[i]
+      const y = array[i + 1]
+      const z = array[i + 2]
+
+      array[i] = x + (Math.random() - 0.5) * 3
+      array[i + 1] = y + (Math.random() - 0.5) * 3
+      array[i + 2] = z + (Math.random() - 0.5) * 3
+    }
+
+    randomValues.push(Math.random() * Math.PI * 2)
+  }
+
+  planeMesh.geometry.attributes.position.randomValues = randomValues
+  planeMesh.geometry.attributes.position.originalPosition =
+    planeMesh.geometry.attributes.position.array
+
+  const colors = []
+  for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(0, 0.19, 0.4)
+  }
+
+  planeMesh.geometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+  )
+}
+
+const raycaster = new THREE.Raycaster()
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+)
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio)
+document.body.appendChild(renderer.domElement)
+
 new OrbitControls(camera, renderer.domElement)
-//Posicionamento para ver o que vai ser renderizado
-camera.position.z = 5;
+camera.position.z = 50
 
-//Criando o plano geometrico
-const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
-//Definindo a cor, com iluminação
+const planeGeometry = new THREE.PlaneGeometry(
+  world.plane.width,
+  world.plane.height,
+  world.plane.widthSegments,
+  world.plane.heightSegments
+)
 const planeMaterial = new THREE.MeshPhongMaterial({
-  color: 0xFF0000,
-  side: THREE.DoubleSide, //Deixando os dois lados visiveis
-  flatShading: THREE.FlatShading //"pixelando"
-});
-//Criando o Mesh do plano
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
+  vertexColors: true
+})
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
-//Adicionando na Scene
-scene.add(planeMesh);
+scene.add(planeMesh)
+generatePlane()
 
-//Alterando o formato, mexendo no array position dentro de geometry no planeMesh
-const {array} = planeMesh.geometry.attributes.position;
-for(let i = 0; i < array.length; i += 3){
-    //Posições individuas de cada vertice
-    const x = array[i];
-    const y = array[i + 1]; 
-    const z = array[i + 2]; 
-    
-    //Movimentando de forma randomica
-    array[i + 2] = z + Math.random();
-} 
-
-
-//Adicionando Iluminação - Cor e intensidade
 const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, -1, 1)
+scene.add(light)
+
 const backLight = new THREE.DirectionalLight(0xffffff, 1)
-//Posicionando a luz eixos x y z
-light.position.set(0, 0, 1);
-backLight.position.set(0, 0, -1);
-//Adicionando a luz a scene
-scene.add(light);
-scene.add(backLight);
-//Criando objeto do mouse
+backLight.position.set(0, 0, -1)
+scene.add(backLight)
+
 const mouse = {
   x: undefined,
   y: undefined
 }
 
-function animate(){
-  requestAnimationFrame(animate);
-  //Renderizando a Scene, varias vezes
-  renderer.render(scene, camera);
-
+let frame = 0
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
   raycaster.setFromCamera(mouse, camera)
-  const intersects = raycaster.intersectObject(planeMesh);
-  if(intersects.length > 0){
-    console.log('intersecting')
+  frame += 0.01
+
+  const {
+    array,
+    originalPosition,
+    randomValues
+  } = planeMesh.geometry.attributes.position
+  for (let i = 0; i < array.length; i += 3) {
+    // x
+    array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.01
+
+    // y
+    array[i + 1] =
+      originalPosition[i + 1] + Math.sin(frame + randomValues[i + 1]) * 0.001
+  }
+
+  planeMesh.geometry.attributes.position.needsUpdate = true
+
+  const intersects = raycaster.intersectObject(planeMesh)
+  if (intersects.length > 0) {
+    const { color } = intersects[0].object.geometry.attributes
+
+    // vertice 1
+    color.setX(intersects[0].face.a, 0.1)
+    color.setY(intersects[0].face.a, 0.5)
+    color.setZ(intersects[0].face.a, 1)
+
+    // vertice 2
+    color.setX(intersects[0].face.b, 0.1)
+    color.setY(intersects[0].face.b, 0.5)
+    color.setZ(intersects[0].face.b, 1)
+
+    // vertice 3
+    color.setX(intersects[0].face.c, 0.1)
+    color.setY(intersects[0].face.c, 0.5)
+    color.setZ(intersects[0].face.c, 1)
+
+    intersects[0].object.geometry.attributes.color.needsUpdate = true
+
+    const initialColor = {
+      r: 0,
+      g: 0.19,
+      b: 0.4
+    }
+
+    const hoverColor = {
+      r: 0.1,
+      g: 0.5,
+      b: 1
+    }
+
+    gsap.to(hoverColor, {
+      r: initialColor.r,
+      g: initialColor.g,
+      b: initialColor.b,
+      duration: 1,
+      onUpdate: () => {
+        // vertice 1
+        color.setX(intersects[0].face.a, hoverColor.r)
+        color.setY(intersects[0].face.a, hoverColor.g)
+        color.setZ(intersects[0].face.a, hoverColor.b)
+
+        // vertice 2
+        color.setX(intersects[0].face.b, hoverColor.r)
+        color.setY(intersects[0].face.b, hoverColor.g)
+        color.setZ(intersects[0].face.b, hoverColor.b)
+
+        // vertice 3
+        color.setX(intersects[0].face.c, hoverColor.r)
+        color.setY(intersects[0].face.c, hoverColor.g)
+        color.setZ(intersects[0].face.c, hoverColor.b)
+        color.needsUpdate = true
+      }
+    })
   }
 }
 
-animate();
+animate()
 
-//Efeito hover
-addEventListener('mousemove', (event)=>{
-  //Definindo o valor de x pela posição do mouse
+addEventListener('mousemove', (event) => {
   mouse.x = (event.clientX / innerWidth) * 2 - 1
-  //Definindo o valor de y pela posição do mouse
   mouse.y = -(event.clientY / innerHeight) * 2 + 1
-});
+})
